50cd279f202c10e6541e69eacc777b08c79bb297
MypkaXD <thuwashev2016@ayndex.ru>
2022-12-27 12:47:20 +0300

    small fix

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -7,7 +7,5 @@ int main(int argc, char** argv)
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 
-	system("pause");
-	std:: cout << "Для продолжения работы программы нажмите любую клавишу...";
-	std::cin.get();
+	return 0;
 }
